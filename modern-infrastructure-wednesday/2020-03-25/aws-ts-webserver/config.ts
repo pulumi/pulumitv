d0f1fa5b7f7178d901673a6900d6eb2c9a5f33b3
Lee Zen <leezen@gmail.com>
2020-03-25 10:46:05 -0700

    Update modern-infrastructure-wednesday/2020-03-25/aws-ts-webserver/config.ts
    Co-Authored-By: Cameron Stokes <cameron@pulumi.com>

--- modern-infrastructure-wednesday/2020-03-25/aws-ts-webserver/config.ts
+++ modern-infrastructure-wednesday/2020-03-25/aws-ts-webserver/config.ts
@@ -14,7 +14,6 @@ export const amiId = aws.getAmi({
     owners: ["099720109477"], // Ubuntu
     mostRecent: true,
     filters: [
-        // { name: "name", values: ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"] },
         { name: "name", values: ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"] },
     ],
 }).id;
