a64394a455ad0f29818fc9ad5cfb071143154169
Lee Zen <leezen@gmail.com>
2020-03-25 10:48:38 -0700

    Rename MIACW to MIW and add 2020-03-25 example (#1)
    * Rename MIACW to MIW and add 2020-03-25 example
    
    Co-Authored-By: Cameron Stokes <cameron@pulumi.com>

---
+++ modern-infrastructure-wednesday/2020-03-25/aws-ts-webserver/policy-as-code/index.ts
@@ -0,0 +1,68 @@
+import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+import { PolicyPack, ReportViolation, validateTypedResource } from "@pulumi/policy";
+
+const policies = new PolicyPack("ec2", {
+    policies: [
+        {
+            name: "discouraged-ec2-public-ip-address",
+            description: "Associating public IP addresses is discouraged.",
+            enforcementLevel: "advisory",
+            // check each resource of type aws.ec2.Instance
+            validateResource: validateTypedResource(aws.ec2.Instance, (it, _, reportViolation) => {
+                if (it.associatePublicIpAddress !== false) {
+                    reportViolation("`associatePublicIpAddresss` should be false");
+                }
+            }),
+        },
+        {
+            name: "required-tags",
+            description: "The folowing tags are required: Name, project, stack.",
+            enforcementLevel: "mandatory",
+            // check each resource of type aws.ec2.Instance
+            validateResource: validateTypedResource(aws.ec2.Instance, (it, _, reportViolation) => {
+                const tags = it.tags || {};
+
+                const projectName = pulumi.getProject();
+                if (tags["project"] !== projectName) {
+                    reportViolation(`[project] tag doesn't match [${projectName}]`);
+                }
+
+                const stackName = pulumi.getStack();
+                if (tags["stack"] !== stackName) {
+                    reportViolation(`[stack] tag doesn't match [${stackName}]`);
+                }
+
+                if (tags["Name"] === undefined) {
+                    reportViolation(`[Name] tag is undefined`);
+                }
+            }),
+        },
+        {
+            name: "discouraged-public-internet",
+            description: "Ingress rules with public internet access are discouraged.",
+            enforcementLevel: "advisory",
+            // check each resource of type aws.ec2.SecurityGroup
+            validateResource: validateTypedResource(aws.ec2.SecurityGroup, (it, _, reportViolation) => {
+                (it.ingress || []).forEach(ingressRule =>
+                    (ingressRule.cidrBlocks || []).forEach(cidr => {
+                        if ("0.0.0.0/0" === cidr) {
+                            reportViolation("`cidr` must not be 0.0.0.0/0")
+                        }
+                    })
+                )
+            }),
+        },
+        {
+            name: "prohibited-services-iot",
+            description: "Use of IOT services is prohibited.",
+            enforcementLevel: "mandatory",
+            // check each resource to validate resource package (e.g. service)
+            validateResource: (args, reportViolation) => {
+                if (args.type.startsWith("aws:iot")) {
+                    reportViolation(`[${args.type}] was found`);
+                }
+            },
+        },
+    ],
+});
