b2e31bf4937fdf3fd3fa7f5f536227c3d5e9ce51
Lee-Ming Zen <lee@pulumi.com>
2020-03-25 10:48:09 -0700

    Remove unused files

--- modern-infrastructure-wednesday/2020-03-25/aws-ts-webserver/elasticbeanstalk.ts
+++
@@ -1,8 +0,0 @@
-import * as aws from "@pulumi/aws";
-
-/**
- * This resource exists to demonstrate policy-as-code.
- */
-export const beanstalkApp = new aws.elasticbeanstalk.Application("main", {
-    description: "My application",
-});
