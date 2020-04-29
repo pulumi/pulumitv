188ccd2cdf8e50ea14cb80f5492e525f7b2ba2c0
Lee Zen <lee@pulumi.com>
2020-04-28 21:12:38 -0700

    MIW 2020-04-29 (#11)

---
+++ modern-infrastructure-wednesday/2020-04-29/webapp/app/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let [status, setStatus] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const s = await fetch("https://webapp.pulumi.tv/api");
+      setStatus(await s.json());
+      console.log(s);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Counter: {status?.count}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
