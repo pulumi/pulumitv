cda352007a8f56621249a9f403c9ed7b5bfc5d5d
Lee-Ming Zen <lee@pulumi.com>
2020-05-19 21:20:54 -0700

    Serverless GraphQL Example for MIW 2020-05-20

---
+++ modern-infrastructure-wednesday/2020-05-20/README.md
@@ -0,0 +1,4 @@
+# Modern Infrastructure Wednesday 2020-05-20
+
+This is the example used in this episode: https://www.youtube.com/watch?v=mU5_aclyQR8
+Watch and follow along!
---
+++ modern-infrastructure-wednesday/2020-05-20/graphql-api/index.ts
@@ -0,0 +1,42 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+import { ApolloServer, gql } from "apollo-server-cloud-functions";
+import * as express from "express";
+
+function factory() {
+    // Construct a schema, using GraphQL schema language
+    const typeDefs = gql`
+        type Query {
+            hello: String
+        }
+    `;
+
+    // Provide resolver functions for your schema fields
+    const resolvers = {
+        Query: {
+            hello: () => 'Hello world!',
+        },
+    };
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        playground: true,
+        introspection: true,
+    });
+
+    const cb = server.createHandler();
+    return cb;
+}
+
+const apiFunction = new gcp.cloudfunctions.HttpCallbackFunction("apiFunction", {
+    callbackFactory: factory,
+});
+
+const apiInvoker = new gcp.cloudfunctions.FunctionIamMember("apiInvoker", {
+    cloudFunction: apiFunction.function.id,
+    member: "allUsers",
+    role: "roles/cloudfunctions.invoker",
+});
+
+export const url = apiFunction.httpsTriggerUrl;
