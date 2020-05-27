7fe2d42bfca41af4f478da33a50bf1730baa003d
Lee-Ming Zen <lee@pulumi.com>
2020-05-27 12:16:22 -0700

    MIW 2020-05-27 Example

---
+++ modern-infrastructure-wednesday/2020-05-27/graphql-py/api/index.js
@@ -0,0 +1,24 @@
+const { ApolloServer, gql } = require('apollo-server-cloud-functions');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true,
+});
+
+exports.handler = server.createHandler();
