d1108bc39606aff9aa632f6d563f24a46ea0ae83
Lee-Ming Zen <lee@pulumi.com>
2020-06-30 23:24:36 -0700

    Fix READMEs and update program

---
+++ modern-infrastructure-wednesday/2020-07-01/README.md
@@ -0,0 +1,7 @@
+# Modern Infrastructure Wednesday 2020-07-01
+
+In today's episode, we use GitHub via Pulumi to manage GitHub. We'll show you how to use Pulumi's
+GitHub action. We happen to execute that against a Pulumi program that uses the GitHub provider.
+So, this means you get to have turtles all the way down: managing GitHub via Pulumi on GitHub.
+
+Watch and follow along at https://youtu.be/yXyDst1RKBY
---
+++ modern-infrastructure-wednesday/2020-07-01/gitception/MyStack.cs
@@ -0,0 +1,21 @@
+using Pulumi;
+using Github = Pulumi.Github;
+
+class MyStack : Stack
+{
+    public MyStack()
+    {
+        var repos = Github.GetRepositories.InvokeAsync(new Github.GetRepositoriesArgs
+        {
+            Query = "org:pulumi gitception",
+        });
+ 
+        foreach (string repo in repos.Result.Names) {
+            var label = new Github.IssueLabel($"{repo}-example-label", new Github.IssueLabelArgs{
+                Repository=repo,
+                Color="512668",
+                Name="pulumitv"
+            });
+        }
+    }
+}
